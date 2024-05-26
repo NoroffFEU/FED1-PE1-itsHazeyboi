75d8feb18eb4f264558f82a156306b8d694c2b26
Haze <117860981+itsHazeyboi@users.noreply.github.com>
2024-05-26 21:01:42 +0200

    Update windowUrlReplacer.mjs

--- js/features/windowUrlReplacer.mjs
+++ js/features/windowUrlReplacer.mjs
@@ -11,11 +11,12 @@ export function setCorrectPath() {
 export function setCorrectMedia() {
   const gitHubPath = "/norofffeu.github.io/FED1-PE1-itsHazeyboi/";
   const currentPath = window.location.pathname;
+  console.log(currentPath);
   const isExactPath = currentPath === gitHubPath;
 
   if (isExactPath) {
     return "./media/favicons/plus-sign.svg";
   } else {
-    return "dadada";
+    return "../media/favicons/plus-sign.svg";
   }
 }
