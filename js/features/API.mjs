4b7d0d239de9af9c59c2ba126888533d35633a34
Haze <117860981+HansiFED@users.noreply.github.com>
2024-08-19 14:17:28 +0200

    Fixed new User / Endpoint for API

--- js/features/API.mjs
+++ js/features/API.mjs
@@ -1,6 +1,6 @@
-export const blogPostsAPI = "https://v2.api.noroff.dev/blog/posts/Haze";
+export const blogPostsAPI = "https://v2.api.noroff.dev/blog/posts/Admin";
 export const loginAPI = "https://v2.api.noroff.dev/auth/login";
 export const registerAPI = "https://v2.api.noroff.dev/auth/register";
-export const deleteAPI = "https://v2.api.noroff.dev/blog/posts/Haze";
+export const deleteAPI = "https://v2.api.noroff.dev/blog/posts/Admin";
 export const getUserProfile = JSON.parse(localStorage.getItem("userProfile"));
 export const localAccessToken = localStorage.getItem("accessToken");
